04c20fa67464d451ed08ca07633abe6cfadaf966
MilonMridha <milonmridha570@gmail.com>
2022-05-22 03:01:47 +0600

    add summary

--- src/Components/Pages/Home/BussinesSummury.js
+++ src/Components/Pages/Home/BussinesSummury.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import SummuryInfo from './SummuryInfo';
+import flag from '../../../assets/icon/flag.jpg'
+import project from '../../../assets/icon/project.jpg'
+import Client from '../../../assets/icon/client.png'
+import feedback from '../../../assets/icon/feedback.png'
 
 const BussinesSummury = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-5 mb-2 px-5'>
-            <SummuryInfo></SummuryInfo>
-            <SummuryInfo></SummuryInfo>
-            <SummuryInfo></SummuryInfo>
-            <SummuryInfo></SummuryInfo>
+        <div className='py-8'>
+            <h2 className='text-3xl font-bold font-mono'>Business Summary</h2>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-5 mb-2 px-5'>
+                <SummuryInfo img={flag} tittle='105+' info="Countries"></SummuryInfo>
+                <SummuryInfo img={project} tittle='888+' info="Complet Project"></SummuryInfo>
+                <SummuryInfo img={Client} tittle='1000+' info="Happy Clients"></SummuryInfo>
+                <SummuryInfo img={feedback} tittle='556+' info="Feedbacks"></SummuryInfo>
+
+            </div>
         </div>
     );
 };
--- src/Components/Pages/Home/SummuryInfo.js
+++ src/Components/Pages/Home/SummuryInfo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const SummuryInfo = () => {
+const SummuryInfo = ({img, tittle, info}) => {
     return (
         <div className={`card lg:card-side bg-base-100 shadow-xl`}>
             <figure className='pl-5'>
-                <img className='pt-5' src="" alt="Album" />
+                <img className='pt-5' src={img} alt="Album" />
             </figure>
-            <div className="card-body text-white">
-                <h2 className="card-title"></h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-body text-black">
+                <h2 className="card-title">{tittle}</h2>
+                <p>{info}</p>
                 
             </div>
         </div>
