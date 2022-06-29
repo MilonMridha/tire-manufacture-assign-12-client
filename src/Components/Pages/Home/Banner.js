8cc601e0e5cac277b9fd82a8b0e2cb8f1531e2df
MilonMridha <milonmridha570@gmail.com>
2022-06-29 18:17:04 +0600

    add typed

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typed from "react-typed";
 import banner from '../../../assets/banner-tyre.jpg'
 
 const Banner = () => {
@@ -7,8 +8,13 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="md:max-w-md sm:col-span-1">
-                    <h1 className="mb-5 text-6xl font-bold font-mono text-yellow-400">Here's</h1>
-                    <p className="mb-5">Our Aim is to Provide Good Quality Products, Prompt Service Support with Long Vision Reliable Products
+                    <h1 className="mb-5 text-6xl font-bold font-mono text-yellow-400"> <Typed
+                        strings={["Here's Our"]}
+                        typeSpeed={40}
+                        backSpeed={50}
+                        loop
+                    /> </h1>
+                    <p className="mb-5"> Aim is to Provide Good Quality Products, Prompt Service Support with Long Vision Reliable Products
                     </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
--- src/Components/Pages/Home/SummuryInfo.js
+++ src/Components/Pages/Home/SummuryInfo.js
@@ -5,16 +5,6 @@ import CountUp from "react-countup";
 const SummuryInfo = ({img, tittle, count, info}) => {
    
     return (
-        // <div className={`card lg:card-side bg-base-100 shadow-xl`}>
-        //     <figure className='pl-5'>
-        //         <img className='pt-5' src={img} alt="Album" />
-        //     </figure>
-        //     <div className="card-body text-black">
-        //         <h2 className="card-title font-bold text-xl"> <span><CountUp end={count} /> {tittle}</span>  </h2>
-        //         <p className='font-bold'>{info}</p>
-                
-        //     </div>
-        // </div>
         <div className="card w-[100%]  bg-base-100 text-neutral-content mt-5 shadow-xl ">
       <div className="card-body items-center text-center text-black">
         <h2 className="card-title text-2xl">
