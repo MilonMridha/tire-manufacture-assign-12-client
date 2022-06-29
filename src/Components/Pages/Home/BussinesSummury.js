5e97524387d8baade68b787627ba6daee513d5ce
MilonMridha <milonmridha570@gmail.com>
2022-06-29 17:38:32 +0600

    polishing my ste

--- src/Components/Pages/Home/BussinesSummury.js
+++ src/Components/Pages/Home/BussinesSummury.js
@@ -10,10 +10,10 @@ const BussinesSummury = () => {
         <div className='py-8'>
             <h2 className='text-3xl font-bold font-mono'>Business Summary</h2>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-5 mb-2 px-5'>
-                <SummuryInfo img={flag} tittle='105+' info="Countries"></SummuryInfo>
-                <SummuryInfo img={project} tittle='120M+' info="Annual revenue"></SummuryInfo>
-                <SummuryInfo img={Client} tittle='33K+' info="Reviews"></SummuryInfo>
-                <SummuryInfo img={feedback} tittle='55+' info="Tools"></SummuryInfo>
+                <SummuryInfo img={flag} tittle='+' count={99} info="Countries"></SummuryInfo>
+                <SummuryInfo img={project} tittle='M+' count={100} info="Annual revenue"></SummuryInfo>
+                <SummuryInfo img={Client} tittle='K+' count={87} info="Reviews"></SummuryInfo>
+                <SummuryInfo img={feedback} tittle='+' count={90} info="Tools"></SummuryInfo>
 
             </div>
         </div>
